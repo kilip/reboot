ef425479ebb3e0742f24381ef6d60df6155f44f4
Anthonius Munthi <me@itstoni.com>
2024-04-21 12:21:44 +0800

    fix(ci): fixed pwa image build issues

--- pwa/src/util/ensure.ts
+++ pwa/src/util/ensure.ts
@@ -2,7 +2,8 @@ import invariant from "tiny-invariant";
 
 export function ensureEnv<T>(key: string): T {
   const value = process.env[ key ];
-  invariant(value, `Environment variables "${key}" is not configured`);
-
+  if (process.env.NODE_ENV !== 'production') {
+    invariant(value, `Environment variables "${key}" is not configured`);
+  }
   return value as T;
 };
